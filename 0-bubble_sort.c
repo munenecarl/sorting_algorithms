3e48d0cb309864df7a27da042a9ec32aa615ad32
munenecarl <munenecarl@gmail.com>
2023-07-24 21:02:58 +0300

    Revert "added indexation for the last swap"
    This reverts commit 45100e767ac93d3274f2840c8e6c14ed2da49a49.

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,12 +12,10 @@ void bubble_sort(int *array, size_t size)
     int temp;
     size_t i, j;
     int swapped;
-    size_t last_swap;
 
     for (i = 0; i < size - 1; i++)
     {
 	swapped = 0;
-	last_swap = 0;
 	for (j = 0; j < size - i - 1; j++)
 	{
 	    if (array[j] > array[j + 1])
@@ -26,13 +24,10 @@ void bubble_sort(int *array, size_t size)
 		array[j] = array[j + 1];
 		array[j + 1] = temp;
 		swapped = 1;
-		last_swap = j;
 		print_array(array, size);
 	    }
 	}
 	if (swapped == 0)
 	    break;
-
-	size = last_swap + 1;
     }
 }
