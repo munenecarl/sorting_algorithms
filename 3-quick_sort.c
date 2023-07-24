e01d41a0942ecd3c663fabbe7a8eca5bf887fde8
munenecarl <munenecarl@gmail.com>
2023-07-24 17:02:16 +0300

    corrections to quicksort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -32,3 +32,37 @@ void quick_sort_helper(int *array, int low, int high, size_t size)
 	quick_sort_helper(array, pivot + 1, high, size);
     }
 }
+
+/**
+ * partition - sorts the list using the quick sort algorithm
+ * @array: array to be sorted
+ * @low: low index
+ * @high: high index
+ * @size: size of the array
+ * Return: pivot
+ */
+
+int partition(int *array, int low, int high, size_t size)
+{
+    int pivot = array[high];
+    int i = low - 1, j, temp;
+
+    for (j = low; j <= high - 1; j++)
+    {
+	if (array[j] <= pivot)
+	{
+	    i++;
+	    temp = array[i];
+	    array[i] = array[j];
+	    array[j] = temp;
+	    if (i != j)
+		print_array(array, size);
+	}
+    }
+    temp = array[i + 1];
+    array[i + 1] = array[high];
+    array[high] = temp;
+    if (i + 1 != j)
+	print_array(array, size);
+    return (i + 1);
+}
