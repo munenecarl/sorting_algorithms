d5cd71f2d86dd24121c4ebb599378a61f8e51eae
munenecarl <munenecarl@gmail.com>
2023-07-24 20:53:42 +0300

    optimized the bubble sort algorithm to check if list is already sorted

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,9 +11,11 @@ void bubble_sort(int *array, size_t size)
 {
     int temp;
     size_t i, j;
+    int swapped;
 
     for (i = 0; i < size - 1; i++)
     {
+	swapped = 0;
 	for (j = 0; j < size - i - 1; j++)
 	{
 	    if (array[j] > array[j + 1])
@@ -21,8 +23,11 @@ void bubble_sort(int *array, size_t size)
 		temp = array[j];
 		array[j] = array[j + 1];
 		array[j + 1] = temp;
+		swapped = 1;
 		print_array(array, size);
 	    }
 	}
+	if (swapped == 0)
+	    break;
     }
 }
