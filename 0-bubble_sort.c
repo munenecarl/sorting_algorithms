45100e767ac93d3274f2840c8e6c14ed2da49a49
munenecarl <munenecarl@gmail.com>
2023-07-24 21:01:01 +0300

    added indexation for the last swap

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,10 +12,12 @@ void bubble_sort(int *array, size_t size)
     int temp;
     size_t i, j;
     int swapped;
+    size_t last_swap;
 
     for (i = 0; i < size - 1; i++)
     {
 	swapped = 0;
+	last_swap = 0;
 	for (j = 0; j < size - i - 1; j++)
 	{
 	    if (array[j] > array[j + 1])
@@ -24,10 +26,13 @@ void bubble_sort(int *array, size_t size)
 		array[j] = array[j + 1];
 		array[j + 1] = temp;
 		swapped = 1;
+		last_swap = j;
 		print_array(array, size);
 	    }
 	}
 	if (swapped == 0)
 	    break;
+
+	size = last_swap + 1;
     }
 }
